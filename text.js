2c745c6bee7f3d375e546b750acd581902f02ec4
Dikshika Adhikari <deekshikaadhikari@gmail.com>
2026-06-14 15:48:06 +0545

    Add files via upload

---
+++ text.js
@@ -0,0 +1,24 @@
+//console.log("Dikshika Adhikari");
+
+  const name="Prajal"
+  console.log(" name output:", name);
+
+  let email="xyz@gmail.com"
+
+  var sub="abc"
+
+  function functionName( arg1, arg2){
+  return arg1 + arg2
+  }
+
+//let a = 10;
+//let b = 30;
+
+//console.log(a+b);
+
+const a ="50";
+const b ="20";
+const c ="30";
+
+console.log( "false", a < b);
+console.log( "true", a > c);
