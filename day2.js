c13193e7293f48d7c433acf471957e336065e597
Dikshika Adhikari <deekshikaadhikari@gmail.com>
2026-06-15 15:52:36 +0545

    Add files via upload

---
+++ day2.js
@@ -0,0 +1,54 @@
+// if else
+    //let age = 17;
+ //if (age >= 18){
+    //console.log("you are eligible");
+ //} else{
+    //console.log("You are not eligible");
+ //}
+
+//else if
+ //let marks = 70;
+ //if (marks >= 90){
+    //console.log("Grade A");
+ //}else if (marks >= 70){
+    //console.log("grade B")
+ //}else if(marks >= 50){
+   // console.log("Grade C");
+ //}
+ // switch case
+ //const option = 1;
+ //switch (option){
+    //case 1 : {
+    //    console.log("one");
+    //}
+    //break;
+   // case 2 : {
+   //     console.log("two");
+   // }
+ //}
+ // loops
+ let number = 5;
+ for ( let i=1; i<=5; i++){
+    console.log (i);
+ }
+ // while
+ let i=1;
+ while(i<=5){
+    console.log("number", i);
+    i++;
+ }
+ 
+ // sum using loops 
+ let sum = 0;
+ for (let i = 1; i<= 10; i++){
+    sum = sum + i;
+ }
+ console.log(" sum =", sum);
+ 
+ // multiplication using loops
+ let product = 1;
+
+for (let i = 1; i <= 10; i++) {
+    product = product * i;
+}
+console.log("Product =", product);
