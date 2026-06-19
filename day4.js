2fa4b526345ebfd9066f6bd467d2ae88f7882100
Dikshika Adhikari <deekshikaadhikari@gmail.com>
2026-06-19 12:50:58 +0545

    Add files via upload

---
+++ day4.js
@@ -0,0 +1,8 @@
+//DOM 
+let element = document.querySelector("#btn"); 
+console.dir(element);
+
+let elements = document.querySelectorAll(".para");
+console.dir(elements);
+
+console.dir(document.body.firstChild);
